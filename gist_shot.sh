18f991c39a116d2a572480cd4911fe8d5543f66f
Jonathan Leung <me@jonl.org>
2016-06-25 12:22:29 -0400

    add copying github raw URL to clipboard

--- gist_shot.sh
+++ gist_shot.sh
@@ -3,7 +3,15 @@ title=$(date "+%Y-%m-%d@%H-%M-%S%p")
 filename="$title.png"
 
 cd "$folder"
+rm -rf .git
 
 screencapture -i "$filename"
 gistup --private --no-open -- "$filename"
+
+repo_hash=$(git remote get-url origin | egrep -o '[0-9a-f]{5,40}')
+commit_hash=$(git rev-parse HEAD)
+url=https://gist.githubusercontent.com/jonleung/$repo_hash/raw/$commit_hash/$filename
+
+echo $url | pbcopy
+
 rm -rf .git
