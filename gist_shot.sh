3a35dd16a439071998113ba346f7a1cc071a126c
Jonathan Leung <me@jonl.org>
2016-06-25 12:48:34 -0400

    add OS X notifications

--- gist_shot.sh
+++ gist_shot.sh
@@ -7,6 +7,9 @@ cd "$MY_SCREENSHOTS_FOLDER_PATH"
 rm -rf .git
 
 screencapture -i "$filename"
+
+osascript -e 'display notification "" with title "Uploading Screenshot..."'
+
 gistup --private --no-open -- "$filename"
 
 repo_hash=$(git remote get-url origin | egrep -o '[0-9a-f]{5,40}')
@@ -14,5 +17,7 @@ commit_hash=$(git rev-parse HEAD)
 url=https://gist.githubusercontent.com/jonleung/$repo_hash/raw/$commit_hash/$filename
 
 echo $url | pbcopy
+
+osascript -e 'display notification "The URL of your screenshot has been copied to your clipboard!" with title "Screenshot URL Copied to Clipboard!" sound name "Submarine"'
 
 rm -rf .git
